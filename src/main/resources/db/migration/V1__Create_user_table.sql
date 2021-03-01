6f287fa917a41bacf7b81caa9e5039ee183cc9e9
quyueliang <846947743@qq.com>
2021-03-01 18:59:38 +0800

    add flyway support

---
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,10 @@
+create table user
+(
+    id int auto_increment primary key not null,
+    account_id varchar(100),
+    name varchar(50),
+    token varchar(100),
+    gmt_create bigint,
+    gmt_modified bigint,
+    bio varchar(256)
+);
