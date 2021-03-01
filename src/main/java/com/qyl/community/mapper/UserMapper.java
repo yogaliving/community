d8622f8d639d2e7b1e853a8bd0bd4b30e28758d7
quyueliang <846947743@qq.com>
2021-03-01 16:56:57 +0800

    add check cookies

--- src/main/java/com/qyl/community/mapper/UserMapper.java
+++ src/main/java/com/qyl/community/mapper/UserMapper.java
@@ -2,8 +2,11 @@ package com.qyl.community.mapper;
 
 import com.qyl.community.model.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserMapper {
     void insert(User user);
+
+    User findByToken(@Param("token") String token);
 }
